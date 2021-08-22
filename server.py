5ccb2abd3581d0315ee5508bfe3178533a6ece2c
multipassport <mashross@gmail.com>
2021-08-23 00:31:05 +0300

    Enable logging

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
 import aiofiles
 import asyncio
+import logging
 import os
 
 from aiohttp import web
 
 
 async def archivate(request):
+    logging.debug('Handling archive downloading request')
     archive_hash = request.match_info.get('archive_hash')
     photos_path = f'./test_photos/{archive_hash}'
     if not os.path.exists(photos_path):
-        raise web.HTTPBadRequest(text='Archive does not exist of was removed')
+        message = f'Archive {archive_hash} does not exist of was removed'
+        logging.exception(message)
+        raise web.HTTPBadRequest(text=message)
     command = f'zip -jr - {photos_path}'
 
     proc = await asyncio.create_subprocess_shell(
@@ -26,8 +30,10 @@ async def archivate(request):
     await response.prepare(request)
 
     while True:
+        logging.debug('Sending archive chunk')
         chunk = await proc.stdout.read(1024 * 1024)
         if proc.stdout.at_eof():
+            logging.debug('End of the archive')
             break
         await response.write(chunk)
     return response
@@ -40,6 +46,12 @@ async def handle_index_page(request):
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.DEBUG,
+        filename='server.log',
+    )
+
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
